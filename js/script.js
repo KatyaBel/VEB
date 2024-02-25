e79689644d89651c806fe7cac9ff75e8a5088a69
KatyaBel <ekaterina.belyaeva21@mail.ru>
2024-02-25 08:44:00 +0500

    Исправлена часть замечаний
    Изменила размер логотипов, выровнила картинку head при большом масштабе, изменила js код для верхнего меню, поправила адаптив для блоков до "ожидания"

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//сразу при загрузке страницы
+//после загрузки страницы
 window.onload = function() {
     updateState()
 };
@@ -6,37 +6,37 @@ window.onload = function() {
 window.addEventListener('resize', function() {
     updateState()
 });
+//показать/скрыть бургер
+function updateState() {
+    const width = window.innerWidth;
+    let button_open = document.querySelector(".burger_open")
+    let button_close = document.querySelector(".burger_close")
+    let menu = document.querySelector(".menu")
+    if (width >= 1024) {
+        button_open.classList.add("hidden")
+        button_close.classList.add("hidden")
+        menu.classList.remove("hidden")
+    } else {
+        button_open.classList.remove("hidden")
+        button_close.classList.add("hidden")
+        menu.classList.add("hidden")
+    }
+}
 //функция для кнопки-бургер
 function openMenu() {
-    let button_open = document.getElementById('open');
-    let button_close = document.getElementById('close');
-    let menu = document.getElementById('my_menu');
-    button_open.style.display = 'none';
-    button_close.style.display = 'block';
-    menu.style.display = 'flex';
+    let button_open = document.querySelector(".burger_open")
+    let button_close = document.querySelector(".burger_close")
+    let menu = document.querySelector(".menu")
+    button_open.classList.add("hidden")
+    button_close.classList.remove("hidden")
+    menu.classList.remove("hidden")
 }
 //функция для крестика
 function closeMenu() {
-    let button_open = document.getElementById('open');
-    let button_close = document.getElementById('close');
-    let menu = document.getElementById('my_menu');
-    button_open.style.display = 'block';
-    button_close.style.display = 'none';
-    menu.style.display = 'none';
-}
-//проверка размера окна
-function updateState() {
-    const width = window.innerWidth;
-    let button_open = document.getElementById('open');
-    let button_close = document.getElementById('close');
-    let menu = document.getElementById('my_menu');
-    if (width >= 768) {
-        button_open.style.display = 'none';
-        button_close.style.display = 'none';
-        menu.style.display = 'flex';
-    } else {
-        button_open.style.display = 'block';
-        button_close.style.display = 'none';
-        menu.style.display = 'none';
-    }
+    let button_open = document.querySelector(".burger_open")
+    let button_close = document.querySelector(".burger_close")
+    let menu = document.querySelector(".menu")
+    button_open.classList.remove("hidden")
+    button_close.classList.add("hidden")
+    menu.classList.add("hidden")
 }
